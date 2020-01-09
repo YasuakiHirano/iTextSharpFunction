ae507320eedc70aa03baa972b7475084be0490d4
y.hirano <yasuakihappy@gmail.com>
2020-01-09 22:43:06 +0900

    update

--- iTextSharpFunction/iTextSharpFunction.cs
+++ iTextSharpFunction/iTextSharpFunction.cs
@@ -73,6 +73,63 @@ namespace iTextPdfTest
             return response;
         }
 
+        [FunctionName("TestPdfOutputOverwrite")]
+        public HttpResponseMessage TestPdfOutputOverwrite(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test_output_pdf")] HttpRequest req,
+            ILogger log
+        )
+        {
+            EncodingProvider encodingProvider;
+            encodingProvider = CodePagesEncodingProvider.Instance;
+            Encoding.RegisterProvider(encodingProvider);
+
+            // 取得したPDFのサイズでDocument作成
+            var reader = new PdfReader(@"Google.pdf");
+            var doc = new Document(reader.GetPageSize(1));
+            var ms = new MemoryStream();
+
+            //ファイルの出力先を設定
+            var pw = PdfWriter.GetInstance(doc, ms);
+
+            //ドキュメントを開く
+            doc.Open();
+
+            var pdfContentByte = pw.DirectContent;
+
+            // テンプレートのページを追加する
+            doc.NewPage();
+            var page = pw.GetImportedPage(reader, 1);
+            pdfContentByte.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+
+            var bf = BaseFont.CreateFont(@"MS Gothic.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+            // ベースフォントとフォントサイズを指定する。
+            pdfContentByte.SetFontAndSize(bf, 10);
+
+            // 指定箇所に文字列を記述
+            ShowTextAligned(pdfContentByte, 10, 500, "Googleのページやで！");
+            ShowTextAligned(pdfContentByte, 10, 480, "日本語の出力テスト。");
+            DrawLine(pdfContentByte, 2, 451, 840, 451);
+
+            doc.Close();
+
+            // レスポンス作成
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(ms.GetBuffer())
+            };
+
+            //Content-Dispositionをattachmentでファイル名を指定していたらプレビューが開かない
+            //response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            //{
+            //    FileName = "testhoge.pdf"
+            //};
+
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
+
+            return response;
+
+        }
         private void ShowTextAligned(PdfContentByte pdfContentByte, float x, float y, string text, int alignment = Element.ALIGN_LEFT, float rotaion = 0)
         {
             pdfContentByte.BeginText();
